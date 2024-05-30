768ce25f3e23c78a41f2d072dfc78a5ccdb8fd3e
Your Name <you@example.com>
2024-05-30 13:24:55 +0100

    initial commit

---
+++ providerscripts/application/configuration/drupal/InstallVirginDeployment.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+###################################################################################
+# Description: This script will install a virgin copy of drupal
+# Author: Peter Winter
+# Date: 04/01/2017
+##################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#################################################################################
+#################################################################################
+#set -x
+
+version="`/bin/echo ${application} | /usr/bin/awk -F':' '{print $NF}'`"
+
+cd /var/www/html
+
+product="drupal"
+
+if ( [ "${product}" = "drupal" ] )
+then
+	/usr/bin/wget https://ftp.drupal.org/files/projects/${product}-${version}.tar.gz
+	/bin/tar xvfx ${product}-${version}.tar.gz
+	/bin/rm ${product}-${version}.tar.gz
+	/bin/mv ${product}-${version}/* .
+	/bin/mv ${product}-${version}/.* .
+	/bin/rmdir ${product}-${version}
+	/bin/rm -r .git
+	/bin/chown -R www-data:www-data /var/www/html/*
+	cd /home/${SERVER_USER}
+	/bin/echo "1"
+fi
