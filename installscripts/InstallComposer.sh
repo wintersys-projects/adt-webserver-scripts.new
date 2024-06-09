0bd9f3a74295c59e950b2f04d7e14ddf8bce8ebb
Peter Charles Winter <160187405+wintersys-projects@users.noreply.github.com>
2024-06-09 11:28:42 +0100

    Update InstallComposer.sh

--- installscripts/InstallComposer.sh
+++ installscripts/InstallComposer.sh
@@ -1,7 +1,10 @@
+
+/usr/sbin/service crond stop
 /usr/bin/apt update
 /usr/bin/apt install php-cli unzip
 cd ~
 /usr/bin/curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
-HASH=`curl -sS https://composer.github.io/installer.sig`
-php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+HASH=`/usr/bin/curl -sS https://composer.github.io/installer.sig`
+/usr/bin/php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+/usr/bin/php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+/usr/sbin/service crond start
