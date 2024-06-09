9263171fff86601c1c36cb69f18892930d3d273a
Peter Charles Winter <160187405+wintersys-projects@users.noreply.github.com>
2024-06-09 11:24:51 +0100

    Create InstallComposer.sh

---
+++ installscripts/InstallComposer.sh
@@ -0,0 +1,7 @@
+/usr/bin/apt update
+/usr/bin/apt install php-cli unzip
+cd ~
+/usr/bin/curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
