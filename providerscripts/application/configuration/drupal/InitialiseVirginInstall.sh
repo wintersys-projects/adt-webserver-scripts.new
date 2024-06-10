874d4a084c3f168079256544ebca27c94ca84569
Peter Charles Winter <160187405+wintersys-projects@users.noreply.github.com>
2024-06-10 17:58:51 +0100

    Update InitialiseVirginInstall.sh

--- providerscripts/application/configuration/drupal/InitialiseVirginInstall.sh
+++ providerscripts/application/configuration/drupal/InitialiseVirginInstall.sh
@@ -44,7 +44,7 @@ then
 	/bin/echo "\$config['system.performance']['css']['preprocess'] = FALSE;" >> /var/www/html/sites/default/settings.php
 	/bin/echo "\$config['system.performance']['js']['preprocess'] = FALSE;" >> /var/www/html/sites/default/settings.php
 	#/bin/echo "\$settings['file_private_path'] = \$app_root . '/sites/default/files/private';" >> /var/www/html/sites/default/settings.php
- 	/bin/echo "\$settings['file_private_path'] = /var/www/private';" >> /var/www/html/sites/default/settings.php
+ 	/bin/echo "\$settings['file_private_path'] = '/var/www/private';" >> /var/www/html/sites/default/settings.php
 	/bin/echo "${0} `/bin/date`: Adjusted the drupal settings: file_private_path, trusted_host_patterns, config_sync_directory, system.performance" >> ${HOME}/logs/OPERATIONAL_MONITORING.log
 	/bin/cp /var/www/html/sites/default/settings.php ${HOME}/runtime/drupal_settings.php 
 fi
