294b3d67f63ae8852d5f6455c4572fd109ff51c8
Peter Charles Winter <160187405+wintersys-projects@users.noreply.github.com>
2024-06-11 12:44:52 +0100

    Update InstallVirginDeployment.sh

--- providerscripts/application/configuration/drupal/InstallVirginDeployment.sh
+++ providerscripts/application/configuration/drupal/InstallVirginDeployment.sh
@@ -53,6 +53,10 @@ then
         /bin/chown www-data:www-data /var/www
         /usr/bin/sudo -u www-data /usr/local/bin/composer update
         /usr/bin/sudo -u www-data /usr/local/bin/composer create-project goalgorilla/social_template:dev-master /tmp/scratch.$$ --with-all-dependencies --no-interaction --working-dir=/tmp/scratch.$$
+        dir="`/usr/bin/pwd`"
+        cd /var/www
+        /usr/bin/sudo -u www-data /usr/local/bin/composer update --with-all-dependencies goalgorilla/open_social
+        cd ${dir}
         /bin/mv /tmp/scratch.$$/* /var/www/
         /bin/rm -r /tmp/scratch.$$
 fi
