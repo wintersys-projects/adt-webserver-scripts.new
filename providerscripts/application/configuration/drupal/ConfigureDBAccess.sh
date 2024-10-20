808e2429031dfd54ea43542c84b08ee24ac420d1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-20 20:06:32 +0100

    Update ConfigureDBAccess.sh

--- providerscripts/application/configuration/drupal/ConfigureDBAccess.sh
+++ providerscripts/application/configuration/drupal/ConfigureDBAccess.sh
@@ -4,19 +4,6 @@ then
 	exit
 fi
 
-if ( [ ! -d  /var/www/private/default_images ] )
-then
-	/bin/mkdir -p /var/www/private/default_images
-fi
-
-#This is the php temporary upload directory
-if ( [ ! -d /var/www/html/tmp ] )
-then
-	/bin/mkdir -p /var/www/html/tmp
- 	/bin/chown www-data:www-data /var/www/html/tmp
-	/bin/chmod 755 /var/www/html/tmp
-fi
-
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
 then
 	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
@@ -44,3 +31,22 @@ then
   	/bin/touch ${HOME}/runtime/DRUPAL_CONFIG_SET
 fi
 
+if ( [ ! -d  /var/www/private/default_images ] )
+then
+	/bin/mkdir -p /var/www/private/default_images
+fi
+
+#This is the php temporary upload directory
+if ( [ ! -d /var/www/html/tmp ] )
+then
+	/bin/mkdir -p /var/www/html/tmp
+ 	/bin/chown www-data:www-data /var/www/html/tmp
+	/bin/chmod 755 /var/www/html/tmp
+fi
+
+if ( [ ! -d /var/www/tmp ] )
+then
+	/bin/mkdir -p /var/www/tmp
+	/bin/chmod 755 /var/www/tmp
+	/bin/chown www-data:www-data /var/www/tmp
+fi
