af778ec745c6b9570edfb8cdef05899ce2018546
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-20 20:00:40 +0100

    Update ConfigureDBAccess.sh

--- providerscripts/application/configuration/drupal/ConfigureDBAccess.sh
+++ providerscripts/application/configuration/drupal/ConfigureDBAccess.sh
@@ -14,11 +14,33 @@ if ( [ ! -d /var/www/html/tmp ] )
 then
 	/bin/mkdir -p /var/www/html/tmp
  	/bin/chown www-data:www-data /var/www/html/tmp
-  /bin/chmod 755 /var/www/html/tmp
+	/bin/chmod 755 /var/www/html/tmp
 fi
 
 if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
 then
 	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
+fi
+
+if ( [ ! -f ${HOME}/runtime/CONFIG_PRIMED ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" = "" ] )
+then
+	${HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh /var/www/html/sites/default/settings.php
+	if ( [ "$?" = "0" ] )
+ 	then
+  		/bin/touch ${HOME}/runtime/CONFIG_PRIMED
+	fi
+fi
+
+if ( [ ! -f ${HOME}/runtime/DRUPAL_CONFIG_SET ] && [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh drupal_settings.php`" != "" ] )
+then	
+	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh drupal_settings.php ${HOME}/runtime/drupal_settings.php
+ 	if ( [ -f /var/www/html/sites/default/settings.php ] )
+  	then
+   		/bin/rm /var/www/html/sites/default/settings.php
+	fi
+ 	/bin/cp ${HOME}/runtime/drupal_settings.php /var/www/html/sites/default/settings.php
+  	/bin/chown www:data-www:data /var/www/html/sites/default/settings.php
+   	/bin/chmod 600 /var/www/html/sites/default/settings.php
+  	/bin/touch ${HOME}/runtime/DRUPAL_CONFIG_SET
 fi
 
