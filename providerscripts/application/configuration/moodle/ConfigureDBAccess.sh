ff4317a671f584e94ba61d44fe10b0a869f7cfd3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-21 11:01:20 +0100

    Update ConfigureDBAccess.sh

--- providerscripts/application/configuration/moodle/ConfigureDBAccess.sh
+++ providerscripts/application/configuration/moodle/ConfigureDBAccess.sh
@@ -1,1 +1,33 @@
+#!/bin/sh
+##################################################################################
+# Description: This script will update update the database credentials for moodle
+# Author: Peter Winter
+# Date: 05/01/2017
+###################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
+
+if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh BUILDARCHIVECHOICE:virgin`" = "1" ] )
+then
+	exit
+fi
+
+if ( [ "`${HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh moodle_config.php`" != "" ] )
+then
+	${HOME}/providerscripts/datastore/configwrapper/GetFromConfigDatastore.sh moodle_config.php ${HOME}/runtime/moodle_config.php.$$
+fi
+
 
