fd4ceeec58a102da8a4688cb21eccc3ac3d7b8ef
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-10-26 14:00:28 +0100

    Update SetupDNSFirewall.sh

--- security/SetupDNSFirewall.sh
+++ security/SetupDNSFirewall.sh
@@ -119,25 +119,23 @@ then
 	fi
 fi
 
-if ( [ "`${HOME}/providerscripts/utilities/CheckConfigValue.sh AUTHORISATIONSERVER:1`" = "0" ] )
+if ( [ "${DNS_CHOICE}" = "digitalocean" ] )
 then
-	if ( [ "${DNS_CHOICE}" = "digitalocean" ] )
-	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
-	fi
-
-	if ( [ "${DNS_CHOICE}" = "exoscale" ] )
-	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
-	fi
-
-	if ( [ "${DNS_CHOICE}" = "linode" ] )
-	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
-	fi
-
-	if ( [ "${DNS_CHOICE}" = "vultr" ] )
-	then
-		/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
-	fi
+	/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
 fi
+
+if ( [ "${DNS_CHOICE}" = "exoscale" ] )
+then
+	/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
+fi
+
+if ( [ "${DNS_CHOICE}" = "linode" ] )
+then
+	/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
+fi
+
+if ( [ "${DNS_CHOICE}" = "vultr" ] )
+then
+	/bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E /usr/sbin/ufw allow 443/tcp
+fi
+
