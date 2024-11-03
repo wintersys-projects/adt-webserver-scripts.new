9718d35d5f851ae7404f91e1039c662b49ee9f17
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-03 23:20:31 +0000

    Update SetupFirewall.sh

--- security/SetupFirewall.sh
+++ security/SetupFirewall.sh
@@ -49,6 +49,9 @@ firewall=""
 if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "FIREWALL" | /usr/bin/awk -F':' '{print $NF}'`" = "ufw" ] )
 then
 	firewall="ufw"
+elif ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "FIREWALL" | /usr/bin/awk -F':' '{print $NF}'`" = "iptables" ] )
+then
+	firewall="iptables"
 fi
 
 if ( [ "${firewall}" = "ufw" ] )
