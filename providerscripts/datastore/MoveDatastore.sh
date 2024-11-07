9dee04586b66a57f8f01ade30b0dbdb897d64cd3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 13:38:54 +0000

    Update MoveDatastore.sh

--- providerscripts/datastore/MoveDatastore.sh
+++ providerscripts/datastore/MoveDatastore.sh
@@ -26,7 +26,10 @@ new_object="$3"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-	/usr/bin/s3cmd mv s3://${original_object} s3://${new_object}
+        datastore_tool="/usr/bin/s3cmd"
 fi
+
+${datastore_tool} mv s3://${original_object} s3://${new_object}
+
 
 
