103bcd34c58bb91bd7f8f94d07d479d9f3fc1ac3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-07 21:36:34 +0000

    Update GetDBCredential.sh

--- providerscripts/datastore/configwrapper/GetDBCredential.sh
+++ providerscripts/datastore/configwrapper/GetDBCredential.sh
@@ -27,10 +27,14 @@ configbucket="${configbucket}-config"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
 then
-        datastore_tool="/usr/bin/s3cmd"
+        datastore_tool="/usr/bin/s3cmd get "
+elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
+then
+        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
 fi
 
-${datastore_tool} --force get s3://${configbucket}/$1 /tmp 1>/dev/null
+${datastore_tool} s3://${configbucket}/$1 /tmp 1>/dev/null
 
 file="`/bin/echo ${1} | /usr/bin/awk -F'/' '{print $NF}'`" 
 
