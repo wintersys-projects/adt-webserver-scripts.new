087d4afccf07690cb325d1affd9430d92c0e62d7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-09 13:42:46 +0000

    Create InstallAll.sh

---
+++ installscripts/InstallAll.sh
@@ -0,0 +1,34 @@
+>&2 /bin/echo "${0} Update.sh"
+${HOME}/installscripts/Update.sh ${BUILDOS}
+
+#>&2 /bin/echo "${0} Upgrade.sh"
+#${HOME}/installscripts/Upgrade.sh ${BUILDOS}
+
+>&2 /bin/echo "${0} InstallCurl.sh"
+${HOME}/installscripts/InstallCurl.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallLibioSocketSSL.sh"
+${HOME}/installscripts/InstallLibioSocketSSL.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallLibnetSSLLeay.sh"
+${HOME}/installscripts/InstallLibnetSSLLeay.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallSendEmail.sh"
+${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallJQ.sh"
+${HOME}/installscripts/InstallJQ.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallUnzip.sh"
+${HOME}/installscripts/InstallUnzip.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallSSHPass.sh"
+${HOME}/installscripts/InstallSSHPass.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallSysStat.sh"
+${HOME}/installscripts/InstallSysStat.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallFirewall.sh"
+${HOME}/installscripts/InstallFirewall.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallS3FS.sh"
+${HOME}/installscripts/InstallS3FS.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallGoofyFS.sh"
+${HOME}/installscripts/InstallGoofyFS.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallRsync.sh"
+${HOME}/installscripts/InstallRsync.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallCron.sh"
+${HOME}/installscripts/InstallCron.sh ${BUILDOS}
+>&2 /bin/echo "${0} InstallGo.sh"
+${HOME}/installscripts/InstallGo.sh ${BUILDOS}
