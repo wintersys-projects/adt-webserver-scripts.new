849f31cecbb0a4414c9247e6f47049d833347d4e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-16 12:04:20 +0000

    Update InstallGoofyFS.sh

--- installscripts/InstallGoofyFS.sh
+++ installscripts/InstallGoofyFS.sh
@@ -28,21 +28,21 @@ if ( [ "${buildos}" = "ubuntu" ] )
 then
 	if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] )
 	then
-		/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys
-		/bin/mv goofys /usr/bin
-		/bin/chmod 755 /usr/bin/goofys
+		/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys			#####UBUNTU-GOOFYS-BINARY#####
+		/bin/mv goofys /usr/bin										#####UBUNTU-GOOFYS-BINARY#####
+		/bin/chmod 755 /usr/bin/goofys									#####UBUNTU-GOOFYS-BINARY#####
 	elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
 	then
-		${HOME}/installscripts/InstallGo.sh ${buildos}
-		DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y install make
-		/usr/bin/git clone https://github.com/kahing/goofys.git
-		cd goofys
-		make install
-		goofys="`/usr/bin/find / -type f -name "goofys" -print`"
-		/bin/mv ${goofys} /usr/bin
-		/bin/chmod 755 /usr/bin/goofys
-		cd ..
-		rm -r ./goofys
+		${HOME}/installscripts/InstallGo.sh ${buildos}							
+		DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y install make	#####UBUNTU-GOOFYS-SOURCE#####
+		/usr/bin/git clone https://github.com/kahing/goofys.git						#####UBUNTU-GOOFYS-SOURCE#####
+		cd goofys											#####UBUNTU-GOOFYS-SOURCE#####
+		make install											#####UBUNTU-GOOFYS-SOURCE#####
+		goofys="`/usr/bin/find / -type f -name "goofys" -print`"					#####UBUNTU-GOOFYS-SOURCE#####
+		/bin/mv ${goofys} /usr/bin									#####UBUNTU-GOOFYS-SOURCE#####
+		/bin/chmod 755 /usr/bin/goofys									#####UBUNTU-GOOFYS-SOURCE#####
+		cd ..												#####UBUNTU-GOOFYS-SOURCE#####
+		rm -r ./goofys											#####UBUNTU-GOOFYS-SOURCE#####
 	fi
 fi
 
@@ -50,20 +50,20 @@ if ( [ "${buildos}" = "debian" ] )
 then
 	if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] )
 	then
-		/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys
-		/bin/mv goofys /usr/bin
-		/bin/chmod 755 /usr/bin/goofys
+		/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys			#####DEBIAN-GOOFYS-BINARY#####
+		/bin/mv goofys /usr/bin										#####DEBIAN-GOOFYS-BINARY#####
+		/bin/chmod 755 /usr/bin/goofys									#####DEBIAN-GOOFYS-BINARY#####
 	elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
 	then
-		${HOME}/installscripts/InstallGo.sh ${buildos}
-		DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y install make
-		/usr/bin/git clone https://github.com/kahing/goofys.git
-		cd goofys
-		make install
-		goofys="`/usr/bin/find / -type f -name "goofys" -print`"
-		/bin/mv ${goofys} /usr/bin
-		/bin/chmod 755 /usr/bin/goofys
-		cd ..
-		rm -r ./goofys
+		${HOME}/installscripts/InstallGo.sh ${buildos}							
+		DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1  -qq -y install make	#####DEBIAN-GOOFYS-SOURCE#####
+		/usr/bin/git clone https://github.com/kahing/goofys.git						#####DEBIAN-GOOFYS-SOURCE#####
+		cd goofys											#####DEBIAN-GOOFYS-SOURCE#####
+		make install											#####DEBIAN-GOOFYS-SOURCE#####
+		goofys="`/usr/bin/find / -type f -name "goofys" -print`"					#####DEBIAN-GOOFYS-SOURCE#####
+		/bin/mv ${goofys} /usr/bin									#####DEBIAN-GOOFYS-SOURCE#####
+		/bin/chmod 755 /usr/bin/goofys									#####DEBIAN-GOOFYS-SOURCE#####
+		cd ..												#####DEBIAN-GOOFYS-SOURCE#####
+		rm -r ./goofys											#####DEBIAN-GOOFYS-SOURCE#####
 	fi
 fi
