bc0c45d6b04deeef917f4a534f5036ed1f48b396
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-16 19:08:47 +0000

    Update InstallMySQLClient.sh

--- installscripts/InstallMySQLClient.sh
+++ installscripts/InstallMySQLClient.sh
@@ -39,9 +39,9 @@ then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
 		mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"	#####UBUNTU-MYSQLCLIENT-REPO#####
-		/usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 					#####UBUNTU-MYSQLCLIENT-REPO#####
-		DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}				#####UBUNTU-MYSQLCLIENT-REPO#####
-		/bin/rm ${mysql_apt_config}									#####UBUNTU-MYSQLCLIENT-REPO#####
+		/usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 					#####UBUNTU-MYSQLCLIENT-REPO-SKIP#####
+		DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}				#####UBUNTU-MYSQLCLIENT-REPO-SKIP#####
+		/bin/rm ${mysql_apt_config}									#####UBUNTU-MYSQLCLIENT-REPO-SKIP#####
         	${HOME}/installscripts/Update.sh ${buildos}							#####UBUNTU-MYSQLCLIENT-REPO#####
 		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=60 -qq -y install mysql-client	#####UBUNTU-MYSQLCLIENT-REPO#####
 	fi
@@ -49,9 +49,9 @@ then
 	if ( [ "${buildos}" = "debian" ] )
 	then
 		mysql_apt_config="`/usr/bin/wget -O- https://dev.mysql.com/downloads/repo/apt/ | /bin/grep -o mysql-apt-config.* | /usr/bin/head -1 | /bin/sed 's/deb.*/deb/g'`"	#####DEBIAN-MYSQLCLIENT-REPO#####
-		/usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 					#####DEBIAN-MYSQLCLIENT-REPO#####
-		DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}				#####DEBIAN-MYSQLCLIENT-REPO#####
-		/bin/rm ${mysql_apt_config}									#####DEBIAN-MYSQLCLIENT-REPO#####
+		/usr/bin/wget https://dev.mysql.com/get/${mysql_apt_config} 					#####DEBIAN-MYSQLCLIENT-REPO-SKIP#####
+		DEBIAN_FRONTEND=noninteractive /usr/bin/dpkg -i ${mysql_apt_config}				#####DEBIAN-MYSQLCLIENT-REPO-SKIP#####
+		/bin/rm ${mysql_apt_config}									#####DEBIAN-MYSQLCLIENT-REPO-SKIP#####
  		${HOME}/installscripts/Update.sh ${buildos}							#####DEBIAN-MYSQLCLIENT-REPO#####
 		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=60 -qq -y install mysql-client	#####DEBIAN-MYSQLCLIENT-REPO#####
 	fi
