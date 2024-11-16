972da336927a60bc8403fc185176863b4ad7dbf4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-16 11:51:10 +0000

    Update InstallAutoSSH.sh

--- installscripts/InstallAutoSSH.sh
+++ installscripts/InstallAutoSSH.sh
@@ -38,12 +38,12 @@ if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install autossh
+		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install autossh	#####UBUNTU-AUTOSSH-REPO#####
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install autossh
+		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install autossh	#####DEBIAN-AUTOSSH-REPO#####
 	fi
 fi
 
