811c2c8d67917fcb8c1d923809a2533e87aaacff
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-20 12:49:06 +0000

    Update BuildLighttpdFromSource.sh

--- installscripts/lighttpd/BuildLighttpdFromSource.sh
+++ installscripts/lighttpd/BuildLighttpdFromSource.sh
@@ -59,12 +59,12 @@ cd lighttpd${major_version}-lighttpd-${minor_version}
 ./autogen.sh
 
 #Get any lise of custom mulues that we are installing and compile with the custom modules if there are any or compile a default build if not
-modules="`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "LIGHTTPD" "stripped" | /bin/sed 's/:/ /g' | /bin/sed 's/source//g'`"
+lighttpd_modules="`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "LIGHTTPD" "stripped" | /bin/sed 's/:/ /g' | /bin/sed 's/source//g'`"
 
-if ( [ "${modules}" != "" ] )
+if ( [ "${lighttpd_modules}" != "" ] )
 then
 	with_modules=""
-	for module in ${modules}
+	for module in ${lighttpd_modules}
 	do
 		with_modules=${with_modules}" --with-${module} "
 	done
