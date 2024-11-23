492025d0a54a0a86a24ee603619e955a4d9e6c10
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-23 12:04:57 +0000

    Update GetPublicIP.sh

--- providerscripts/utilities/GetPublicIP.sh
+++ providerscripts/utilities/GetPublicIP.sh
@@ -21,6 +21,13 @@
 #set -x
 
 IP="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'MYPUBLICIP'`"
+
+if ( [ "`/usr/bin/hostname -I | /bin/grep ${IP}`" = "" ] )
+then
+	IP="`/usr/bin/hostname -I | /usr/bin/awk '{print $1}'`"
+	${HOME}/providerscripts/utilities/StoreConfigValue.sh 'MYIP' "${IP}"
+fi
+
 /bin/echo ${IP}
 
 
