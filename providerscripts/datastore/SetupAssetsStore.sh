7f66bb59da95f99742737530d3c5334cd9dba624
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-26 18:05:43 +0000

    Update SetupAssetsStore.sh

--- providerscripts/datastore/SetupAssetsStore.sh
+++ providerscripts/datastore/SetupAssetsStore.sh
@@ -98,7 +98,7 @@ BUILDOS="`${HOME}/providerscripts/utilities/ExtractConfigValue.sh 'BUILDOS'`"
 
 for assetbucket in ${applicationassetbuckets}
 do
-	assetbuckets="${assetbuckets} `/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`-${assetbucket}"
+	assetbuckets="${assetbuckets} `/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${assetbucket}"
 done
 
 export AWSACCESSKEYID=`/bin/grep 'access_key' ~/.s3cfg | /usr/bin/awk '{print $NF}'`
