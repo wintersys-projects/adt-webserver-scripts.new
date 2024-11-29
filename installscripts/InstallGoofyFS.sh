5ccefe4d6f9d188253350e6d34c2ac03e726dff6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-29 19:21:22 +0000

    Update InstallGoofyFS.sh

--- installscripts/InstallGoofyFS.sh
+++ installscripts/InstallGoofyFS.sh
@@ -24,72 +24,85 @@ then
 	buildos="${1}"
 fi
 
-if ( [ "${buildos}" = "ubuntu" ] )
+apt=""
+if ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
-	if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] )
-	then
-		/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys -P /usr/bin	#####UBUNTU-GOOFYS-BINARY#####
-		/bin/chmod 755 /usr/bin/goofys									#####UBUNTU-GOOFYS-BINARY#####
-	elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
-	then
-		if ( [ -d /root/scratch ] )                                             #####UBUNTU-GOOFYS-SOURCE#####
-		then                                                                    #####UBUNTU-GOOFYS-SOURCE#####
-        		/bin/rm -r /root/scratch                                        #####UBUNTU-GOOFYS-SOURCE#####
-		else                                                                    #####UBUNTU-GOOFYS-SOURCE#####
-        		/bin/mkdir /root/scratch                                        #####UBUNTU-GOOFYS-SOURCE#####
-		fi                                                                      #####UBUNTU-GOOFYS-SOURCE#####
-
-		cwd="`/usr/bin/pwd`"                                                    #####UBUNTU-GOOFYS-SOURCE#####
-        	DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install make    #####UBUNTU-GOOFYS-SOURCE#####
-        	/usr/bin/git clone https://github.com/kahing/goofys.git /root/scratch                                           #####UBUNTU-GOOFYS-SOURCE#####
-        	cd /root/scratch                                                                                        #####UBUNTU-GOOFYS-SOURCE#####
-        	/usr/bin/make install                                                                                   #####UBUNTU-GOOFYS-SOURCE#####
-
-        	if ( [ -f /root/go/bin/goofys ] )                                                                       #####UBUNTU-GOOFYS-SOURCE#####
-        	then                                                                                                    #####UBUNTU-GOOFYS-SOURCE#####
-                	/bin/mv /root/go/bin/goofys /usr/bin                                                                    #####UBUNTU-GOOFYS-SOURCE-SKIP#####
-                	/bin/chmod 755 /usr/bin/goofys                                                                  #####UBUNTU-GOOFYS-SOURCE#####
-        	fi                                                                                                      #####UBUNTU-GOOFYS-SOURCE#####
-
-        	if ( [ -d /root/scratch ] )                                                                             #####UBUNTU-GOOFYS-SOURCE#####
-        	then                                                                                                    #####UBUNTU-GOOFYS-SOURCE#####
-                	/bin/rm -r /root/scratch                                                                        #####UBUNTU-GOOFYS-SOURCE#####
-        	fi                                                                                                      #####UBUNTU-GOOFYS-SOURCE#####
-		cd ${cwd}
-	fi
+	apt="/usr/bin/apt-get"
+elif ( [ "`${HOME}/providerscripts/utilities/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt-fast" ] )
+then
+	apt="/usr/sbin/apt-fast"
 fi
 
-if ( [ "${buildos}" = "debian" ] )
+if ( [ "${apt}" != "" ] )
 then
-        if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] )
-        then
-                /usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys -P /usr/bin      #####DEBIAN-GOOFYS-BINARY#####
-                /bin/chmod 755 /usr/bin/goofys                                                                  #####DEBIAN-GOOFYS-BINARY#####
-        elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
-        then
-                if ( [ -d /root/scratch ] )                                             #####DEBIAN-GOOFYS-SOURCE#####
-                then                                                                    #####DEBIAN-GOOFYS-SOURCE#####
-                        /bin/rm -r /root/scratch                                        #####DEBIAN-GOOFYS-SOURCE#####
-                else                                                                    #####DEBIAN-GOOFYS-SOURCE#####
-                        /bin/mkdir /root/scratch                                        #####DEBIAN-GOOFYS-SOURCE#####
-                fi                                                                      #####DEBIAN-GOOFYS-SOURCE#####
+	if ( [ "${buildos}" = "ubuntu" ] )
+	then
+		if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] )
+		then
+			/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys -P /usr/bin	#####UBUNTU-GOOFYS-BINARY#####
+			/bin/chmod 755 /usr/bin/goofys									#####UBUNTU-GOOFYS-BINARY#####
+		elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
+		then
+			if ( [ -d /root/scratch ] )                                             #####UBUNTU-GOOFYS-SOURCE#####
+			then                                                                    #####UBUNTU-GOOFYS-SOURCE#####
+        			/bin/rm -r /root/scratch                                        #####UBUNTU-GOOFYS-SOURCE#####
+			else                                                                    #####UBUNTU-GOOFYS-SOURCE#####
+        			/bin/mkdir /root/scratch                                        #####UBUNTU-GOOFYS-SOURCE#####
+			fi                                                                      #####UBUNTU-GOOFYS-SOURCE#####
 
-                cwd="`/usr/bin/pwd`"                                                    #####DEBIAN-GOOFYS-SOURCE#####
-                DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install make    #####DEBIAN-GOOFYS-SOURCE#####
-                /usr/bin/git clone https://github.com/kahing/goofys.git /root/scratch                                           #####DEBIAN-GOOFYS-SOURCE#####
-                cd /root/scratch                                                                                        #####DEBIAN-GOOFYS-SOURCE#####
-                /usr/bin/make install                                                                                   #####DEBIAN-GOOFYS-SOURCE#####
+			cwd="`/usr/bin/pwd`"                                                    #####UBUNTU-GOOFYS-SOURCE#####
+        		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install make    #####UBUNTU-GOOFYS-SOURCE#####
+        		/usr/bin/git clone https://github.com/kahing/goofys.git /root/scratch                                           #####UBUNTU-GOOFYS-SOURCE#####
+        		cd /root/scratch                                                                                        #####UBUNTU-GOOFYS-SOURCE#####
+        		/usr/bin/make install                                                                                   #####UBUNTU-GOOFYS-SOURCE#####
 
-                if ( [ -f /root/go/bin/goofys ] )                                                                       #####DEBIAN-GOOFYS-SOURCE#####
-                then                                                                                                    #####DEBIAN-GOOFYS-SOURCE#####
-                        /bin/mv /root/go/bin/goofys /usr/bin                                                                    #####DEBIAN-GOOFYS-SOURCE-SKIP#####
-                        /bin/chmod 755 /usr/bin/goofys                                                                  #####DEBIAN-GOOFYS-SOURCE#####
-                fi                                                                                                      #####DEBIAN-GOOFYS-SOURCE#####
+        		if ( [ -f /root/go/bin/goofys ] )                                                                       #####UBUNTU-GOOFYS-SOURCE#####
+        		then                                                                                                    #####UBUNTU-GOOFYS-SOURCE#####
+                		/bin/mv /root/go/bin/goofys /usr/bin                                                                    #####UBUNTU-GOOFYS-SOURCE-SKIP#####
+                		/bin/chmod 755 /usr/bin/goofys                                                                  #####UBUNTU-GOOFYS-SOURCE#####
+        		fi                                                                                                      #####UBUNTU-GOOFYS-SOURCE#####
 
-                if ( [ -d /root/scratch ] )                                                                             #####DEBIAN-GOOFYS-SOURCE#####
-                then                                                                                                    #####DEBIAN-GOOFYS-SOURCE#####
-                        /bin/rm -r /root/scratch                                                                        #####DEBIAN-GOOFYS-SOURCE#####
-                fi                                                                                                      #####DEBIAN-GOOFYS-SOURCE#####
-                cd ${cwd}
+        		if ( [ -d /root/scratch ] )                                                                             #####UBUNTU-GOOFYS-SOURCE#####
+        		then                                                                                                    #####UBUNTU-GOOFYS-SOURCE#####
+                		/bin/rm -r /root/scratch                                                                        #####UBUNTU-GOOFYS-SOURCE#####
+        		fi                                                                                                      #####UBUNTU-GOOFYS-SOURCE#####
+			cd ${cwd}
+		fi
+	fi
+
+	if ( [ "${buildos}" = "debian" ] )
+	then
+        	if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:binary'`" = "1" ] )
+        	then
+                	/usr/bin/wget https://github.com/kahing/goofys/releases/latest/download/goofys -P /usr/bin      #####DEBIAN-GOOFYS-BINARY#####
+                	/bin/chmod 755 /usr/bin/goofys                                                                  #####DEBIAN-GOOFYS-BINARY#####
+        	elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTOREMOUNTTOOL:goof:source'`" = "1" ] )
+        	then
+                	if ( [ -d /root/scratch ] )                                             #####DEBIAN-GOOFYS-SOURCE#####
+                	then                                                                    #####DEBIAN-GOOFYS-SOURCE#####
+                        	/bin/rm -r /root/scratch                                        #####DEBIAN-GOOFYS-SOURCE#####
+                	else                                                                    #####DEBIAN-GOOFYS-SOURCE#####
+                        	/bin/mkdir /root/scratch                                        #####DEBIAN-GOOFYS-SOURCE#####
+                	fi                                                                      #####DEBIAN-GOOFYS-SOURCE#####
+
+                	cwd="`/usr/bin/pwd`"                                                    #####DEBIAN-GOOFYS-SOURCE#####
+                	DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install make    #####DEBIAN-GOOFYS-SOURCE#####
+                	/usr/bin/git clone https://github.com/kahing/goofys.git /root/scratch                                           #####DEBIAN-GOOFYS-SOURCE#####
+                	cd /root/scratch                                                                                        #####DEBIAN-GOOFYS-SOURCE#####
+                	/usr/bin/make install                                                                                   #####DEBIAN-GOOFYS-SOURCE#####
+
+                	if ( [ -f /root/go/bin/goofys ] )                                                                       #####DEBIAN-GOOFYS-SOURCE#####
+                	then                                                                                                    #####DEBIAN-GOOFYS-SOURCE#####
+                        	/bin/mv /root/go/bin/goofys /usr/bin                                                            #####DEBIAN-GOOFYS-SOURCE-SKIP#####
+                        	/bin/chmod 755 /usr/bin/goofys                                                                  #####DEBIAN-GOOFYS-SOURCE#####
+                	fi                                                                                                      #####DEBIAN-GOOFYS-SOURCE#####
+
+                	if ( [ -d /root/scratch ] )                                                                             #####DEBIAN-GOOFYS-SOURCE#####
+                	then                                                                                                    #####DEBIAN-GOOFYS-SOURCE#####
+                        	/bin/rm -r /root/scratch                                                                        #####DEBIAN-GOOFYS-SOURCE#####
+                	fi                                                                                                      #####DEBIAN-GOOFYS-SOURCE#####
+                	cd ${cwd}												#####DEBIAN-GOOFYS-SOURCE#####
+		fi
+   		
         fi
 fi
