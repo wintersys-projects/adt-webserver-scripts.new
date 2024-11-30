23d8f290a2ac95914dc5afbdfc5813610b90ef9c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-30 18:44:34 +0000

    Update InstallSSHPass.sh

--- installscripts/InstallSSHPass.sh
+++ installscripts/InstallSSHPass.sh
@@ -44,6 +44,6 @@ then
 	then
 		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1  -qq -y install sshpass		#####DEBIAN-SSHPASS-REPO#####
 	fi
-      	/bin/touch ${HOME}/runtime/installedsoftware/SSH_PASS				
+      	/bin/touch ${HOME}/runtime/installedsoftware/InstallSSHPass.sh			
 fi
 
