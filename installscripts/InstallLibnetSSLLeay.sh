a0ac01dd6d175dcd387b7b14ca0a38781902d2bc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-11-30 18:41:11 +0000

    Update InstallLibnetSSLLeay.sh

--- installscripts/InstallLibnetSSLLeay.sh
+++ installscripts/InstallLibnetSSLLeay.sh
@@ -44,6 +44,6 @@ then
 	then
 		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install libnet-ssleay-perl #####DEBIAN-LIBNETSSL-REPO#####
 	fi
-      	/bin/touch ${HOME}/runtime/installedsoftware/LIBNET_SSL				
+      	/bin/touch ${HOME}/runtime/installedsoftware/InstallLibnetSSLLeay.sh				
 fi
 
