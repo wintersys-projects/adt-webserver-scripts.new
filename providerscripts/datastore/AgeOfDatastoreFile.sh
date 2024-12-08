ac2df429bcacdbd2b184eaebba58e4a6e8b073b0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-08 11:14:17 +0000

    Update AgeOfDatastoreFile.sh

--- providerscripts/datastore/AgeOfDatastoreFile.sh
+++ providerscripts/datastore/AgeOfDatastoreFile.sh
@@ -3,3 +3,4 @@
 time_file_written="`/usr/bin/s3cmd info s3://$1| grep "Last mod" | /usr/bin/awk -F',' '{print $2}'`"
 time_now="`/usr/bin/date +%s`"
 age_of_file_in_seconds="`/usr/bin/expr ${time_now} - ${time_file_written}`"
+/bin/echo ${age_of_file_in_seconds}
