bc846c1c3506c3f8cd3ea1ad4f0a614b2d83c2d4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-08 11:46:29 +0000

    Update AgeOfDatastoreFile.sh

--- providerscripts/datastore/AgeOfDatastoreFile.sh
+++ providerscripts/datastore/AgeOfDatastoreFile.sh
@@ -1,4 +1,3 @@
-
 inspected_file="${1}"
 
 if ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
@@ -8,6 +7,8 @@ elif ( [ "`${HOME}/providerscripts/utilities/CheckBuildStyle.sh 'DATASTORETOOL:s
 then
   time_file_written="`/usr/bin/s5cmd ls s3://${inspected_file} | /usr/bin/awk '{print $1,$2}'`"
 fi
+
+time_file_written="`/usr/bin/date -d "${time_file_written}" +%s`"
 
 time_now="`/usr/bin/date +%s`"
 age_of_file_in_seconds="`/usr/bin/expr ${time_now} - ${time_file_written}`"
