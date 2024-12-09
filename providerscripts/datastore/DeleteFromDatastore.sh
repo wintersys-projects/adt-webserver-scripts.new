6748766cc300d1568927eb908bbbf9f06e119c96
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-09 18:07:41 +0000

    Update DeleteFromDatastore.sh

--- providerscripts/datastore/DeleteFromDatastore.sh
+++ providerscripts/datastore/DeleteFromDatastore.sh
@@ -32,5 +32,12 @@ then
         datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} rm "
 fi
 
-${datastore_tool} s3://${file_to_delete}
+count="0"
+while ( [ "`${datastore_tool} s3://${file_to_delete} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+do
+        /bin/sleep 5
+        count="`/usr/bin/expr ${count} + 1`"
+done 	
+
+
 
