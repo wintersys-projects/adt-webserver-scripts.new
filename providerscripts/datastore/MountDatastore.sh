56406e99f27e3062c51715b7a8142f8546f23ba0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-10 09:39:16 +0000

    Update MountDatastore.sh

--- providerscripts/datastore/MountDatastore.sh
+++ providerscripts/datastore/MountDatastore.sh
@@ -39,6 +39,7 @@ fi
 count="0"
 while ( [ "`${datastore_tool} mb s3://${datastore_to_mount} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
 do
+        /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
         /bin/sleep 5
         count="`/usr/bin/expr ${count} + 1`"
 done 
