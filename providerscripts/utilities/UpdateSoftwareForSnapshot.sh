08c0352f327f10e40d838be6ac4ffbbe518bc4ce
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-11 21:24:05 +0000

    Update UpdateSoftwareForSnapshot.sh

--- providerscripts/utilities/UpdateSoftwareForSnapshot.sh
+++ providerscripts/utilities/UpdateSoftwareForSnapshot.sh
@@ -1,4 +1,4 @@
-if ( [ -f ${HOME}/runtime/WEBSERVER_UPDATED_FOR_SNAPSHOT ] && [ ! -f ${HOME}/runtime/SOFTWARE_UPDATED_FOR_SNAPSHOT ] )
+if ( [ -f ${HOME}/runtime/APPLICATION_UPDATED_FOR_SNAPSHOT ] && [ ! -f ${HOME}/runtime/SOFTWARE_UPDATED_FOR_SNAPSHOT ] )
 then
     /bin/touch ${HOME}/runtime/SOFTWARE_UPDATED_FOR_SNAPSHOT
     ${HOME}/providerscripts/utilities/UpdateSoftware.sh
