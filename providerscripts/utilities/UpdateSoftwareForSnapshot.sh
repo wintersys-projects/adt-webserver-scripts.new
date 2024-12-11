2d4a89cf8e2f72a90bff8f4c070d329a6284afe9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-11 22:02:59 +0000

    Delete providerscripts/utilities/UpdateSoftwareForSnapshot.sh

--- providerscripts/utilities/UpdateSoftwareForSnapshot.sh
+++
@@ -1,5 +0,0 @@
-if ( [ -f ${HOME}/runtime/APPLICATION_UPDATED_FOR_SNAPSHOT ] && [ ! -f ${HOME}/runtime/SOFTWARE_UPDATED_FOR_SNAPSHOT ] )
-then
-    /bin/touch ${HOME}/runtime/SOFTWARE_UPDATED_FOR_SNAPSHOT
-    ${HOME}/providerscripts/utilities/UpdateSoftware.sh
-fi
