bc53f90e8426fb5794dace2b7d75fef9a1df4af9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-11 12:40:09 +0000

    Update RefreshApplicationForSnapshot.sh

--- providerscripts/application/RefreshApplicationForSnapshot.sh
+++ providerscripts/application/RefreshApplicationForSnapshot.sh
@@ -30,7 +30,7 @@ then
         fi
 fi
 
-if ( [ -f ${HOME}/runtime/APPLICATION_UPDATED_FOR_SNAPSHOT ] )
+if ( [ ! -f ${HOME}/runtime/SNAPSHOT_BUILT ] || [ -f ${HOME}/runtime/APPLICATION_UPDATED_FOR_SNAPSHOT ] )
 then
         exit
 fi
